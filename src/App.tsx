4e84a33badd02d2851d33c7f881edf9fea975bcd
Hugo-Farias <hg80rod@gmail.com>
2023-07-21 00:18:01 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import InputField from "./components/InputField";
+
+function App() {
+  return (
+    <div className="app">
+      <InputField />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,11 @@
+import "./InputField.scss";
+
+function InputField() {
+  return (
+    <div className="input-field">
+      <p>TESdasdasdT</p>
+    </div>
+  );
+}
+
+export default InputField;
