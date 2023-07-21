be9e7e135446f462671fa2d3e80d2bd5bfa8e5b8
Hugo-Farias <hg80rod@gmail.com>
2023-07-21 09:05:01 -0300

    password field completed

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,14 @@
 import "./InputField.scss";
+import copySvg from "../assets/images/icon-copy.svg";
 
 function InputField() {
   return (
     <div className="input-field">
-      <div className="title">Password Generator</div>
+      <h1 className="title">Password Generator</h1>
+      <div className="result">
+        <h2 className="password">PTx1f5DaFX</h2>
+        <img alt="Copy Icon" src={copySvg} />
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 
+if (import.meta.hot) {
+  import.meta.hot.on("vite:beforeUpdate", () => console.clear());
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
