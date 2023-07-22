2bf1ad549008aa79ba3c1278f6f7c2a17ade72f2
Hugo-Farias <hg80rod@gmail.com>
2023-07-22 00:18:34 -0300

    changed Options component to be specific to slider for more flexibility

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,39 +1,10 @@
 import "./Options.scss";
-import { CSSProperties, useRef, useState } from "react";
+import CharacterLength from "./options/CharacterLength";
 
 const Options = function () {
-  const sliderRef = useRef<HTMLInputElement | null>(null);
-  const [currentLen, setCurrentLen] = useState<number>(10);
-  const maxLength = +sliderRef.current?.max || currentLen * 2;
-  const percentage = (currentLen / maxLength) * 100 - 2;
-  const colorPrimary = "#a4ffaf";
-
-  const inputStyle: CSSProperties = {
-    background: `linear-gradient(to right,${colorPrimary} 0,${colorPrimary} ${percentage}%,#18171F ${percentage}%,#18171F 100%)`,
-  };
-
-  const handleInputChange = function () {
-    setCurrentLen(+sliderRef.current?.value);
-  };
-
   return (
     <div className="options">
-      <div className="length-option">
-        <h3 className="name">Character Length</h3>
-        <span className="length-number">{currentLen}</span>
-      </div>
-      <input
-        ref={sliderRef}
-        value={currentLen}
-        onChange={handleInputChange}
-        className="slider"
-        type="range"
-        id="slider"
-        min="1"
-        max="20"
-        step="1"
-        style={inputStyle}
-      />
+      <CharacterLength />
     </div>
   );
 };
